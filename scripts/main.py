62f45d5d69a385c63194b32337c4c18afb3a8fbe
User Lambda <user.l@work.com>
2023-11-12 17:34:56 +0100

    Add sep_line() function

--- scripts/main.py
+++ scripts/main.py
@@ -1,7 +1,25 @@
 #!/usr/bin/env python3
+import sys
+import os
+
+
+def sep_line(width=None, sep='─', file=sys.stdout):
+    """
+    Print a separatory line spanning the whole shell's width
+
+    Keyword arguments:
+    size -- width of the separatory line (default: None)
+    sep  -- character used to build the separatory line (default: U+2500)
+    file -- ioctl device where the line should be printed (default: sys.stdout)
+
+    Return: None
+    """
+    width = os.get_terminal_size().columns if width is None else width
+    print(sep * width, file=file)
 
 def main():
     print("hello git!")
+    sep_line()
 
 if __name__ == '__main__':
     main()
